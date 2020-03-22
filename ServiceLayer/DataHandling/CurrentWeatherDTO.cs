b776d6940856d4adffaedb94e856bb0e94124c36
JayMackay <jared.mackay1994@live.com>
2020-03-22 19:33:54 +0000

    Data Transfer Object To Deserialize API Request

---
+++ ServiceLayer/DataHandling/CurrentWeatherDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace OpenWeatherAPITestProject.ServiceLayer.DataHandling
+{
+    public class CurrentWeatherDTO
+    {
+        //Get current weather root properties
+        public CurrentWeatherRootObject CurrentWeather { get; set; }
+
+        //Create an instance of the Current Weather class once a response is passed via the Open Weather API
+        public void DeserializeWeather(string CurrentWeatherResponse)
+        {
+            CurrentWeather = JsonConvert.DeserializeObject<CurrentWeatherRootObject>(CurrentWeatherResponse);
+        }
+    }
+}
