9b21729e6fb8591eb9bb7d98b141c1e4d064bb11
JayMackay <jared.mackay1994@live.com>
2020-03-22 23:10:53 +0000

    Additional Unit Tests

--- Tests/WeatherRequestTests.cs
+++ Tests/WeatherRequestTests.cs
@@ -15,5 +15,63 @@ namespace OpenWeatherAPITestProject
             Assert.AreEqual(200, currentWeather.currentWeatherDTO.CurrentWeather.cod);
         }
 
+        [Test]
+        public void CurrentCity()
+        {
+            Assert.AreEqual("London", currentWeather.currentWeatherDTO.CurrentWeather.name);
+        }
+
+        [Test]
+        public void CurrentCountry()
+        {
+            Assert.AreEqual("GB", currentWeather.currentWeatherDTO.CurrentWeather.sys.country);
+        }
+
+        [Test]
+        public void HumidtyCheck()
+        {
+            CurrentWeatherService cws = new CurrentWeatherService();
+            var result = cws.HumidCheck();
+            Assert.That(result);
+        }
+
+        [Test]
+        public void TimeZoneCheck()
+        {
+            Assert.AreEqual(0, currentWeather.currentWeatherDTO.CurrentWeather.timezone);
+        }
+
+        [Test]
+        public void LatitudeCheck()
+        {
+            Assert.AreEqual(51.51f, currentWeather.currentWeatherDTO.CurrentWeather.coord.lat);
+        }
+
+        [Test]
+        public void LongititudeCheck()
+        {
+            Assert.AreEqual(-0.13f, currentWeather.currentWeatherDTO.CurrentWeather.coord.lon);
+        }
+
+        [Test]
+        public void WindSpeedCheck()
+        {
+            Assert.AreEqual(6.2f, currentWeather.currentWeatherDTO.CurrentWeather.wind.speed);
+        }
+
+        [Test]
+        public void WebCallFailDueToIncorrectCity()
+        {
+            CurrentWeatherService cw = new CurrentWeatherService();
+            var currentW = cw.weatherCallManager.GetCurrentWeather();
+            var expected = currentW.ToString();
+            Assert.AreEqual(expected, currentW);
+        }
+
+        [Test]
+        public void IdCheck()
+        {
+            Assert.AreEqual(2643743, currentWeather.currentWeatherDTO.CurrentWeather.id);
+        }
     }
 }
